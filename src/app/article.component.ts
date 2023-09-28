3b62932777822b645762e029a736ef45e3da7deb
Topaz712 <tzam712@gmail.com>
2023-09-28 15:12:28 -0500

    bonus: create button to toggle isTechRelated

--- src/app/article.component.ts
+++ src/app/article.component.ts
@@ -8,6 +8,10 @@ export class ArticleComponent {
   title: string = "The Angular Basics Exercise";
   content: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Libero volutpat sed cras ornare arcu dui vivamus. Leo in vitae turpis massa sed elementum tempus egestas sed. Semper auctor neque vitae tempus quam pellentesque nec nam. Convallis posuere morbi leo urna molestie. Quis viverra nibh cras pulvinar mattis nunc sed. Fermentum odio eu feugiat pretium nibh ipsum consequat. Condimentum vitae sapien pellentesque habitant morbi. Augue ut lectus arcu bibendum. Phasellus faucibus scelerisque eleifend donec pretium. In hendrerit gravida rutrum quisque non. Ultricies lacus sed turpis tincidunt id aliquet. Quis auctor elit sed vulputate mi sit amet. Quis lectus nulla at volutpat diam ut venenatis. Imperdiet massa tincidunt nunc pulvinar sapien. Sit amet tellus cras adipiscing enim eu turpis. Erat nam at lectus urna duis convallis convallis tellus id."
 
-  isTechRelate: boolean = true;
+  isTechRelated: boolean = true;
+
+  toggleTechRelated() {
+    this.isTechRelated = !this.isTechRelated;
+  }
 
 }
